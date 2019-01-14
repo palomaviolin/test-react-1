97b462b6644cdeda2177c0fba82b9907241fdd26
Paloma Ania <paloma.aniabenito@gmail.com>
2019-01-14 09:12:48 +0100

    Updates

---
+++ precache-manifest.a883b2f2f61df4cc88d0e808705eac32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-react-1/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c32c3e8a262f6a555276",
+    "url": "/test-react-1/static/js/runtime~main.c32c3e8a.js"
+  },
+  {
+    "revision": "edc482a125bd3929f768",
+    "url": "/test-react-1/static/js/main.edc482a1.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/test-react-1/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "edc482a125bd3929f768",
+    "url": "/test-react-1/static/css/main.d532d734.chunk.css"
+  },
+  {
+    "revision": "93b7c485ec3c70038b20565ad2ba0999",
+    "url": "/test-react-1/index.html"
+  }
+];
